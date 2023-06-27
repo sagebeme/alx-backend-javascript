0ac7e11287d29968f9bac786b6fc1dd64086c1a1
sagebeme <eudicas5k@gmail.com>
2023-06-27 16:12:46 +0300

    concatinating array with string

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,5 @@
 export default function concatArrays(array1, array2, string) {
-    let arry = [...array1, ...array2, ...string]
-    arry.concat(string)
-    return arry
+  const arry = [...array1, ...array2, ...string];
+  arry.concat(string);
+  return arry;
 }
